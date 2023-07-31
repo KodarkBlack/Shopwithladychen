439fe403c99f74ac2a48382dbacc675d360502ca
Olufisayo Israel Tomide <115098511+KodarkBlack@users.noreply.github.com>
2023-07-31 18:33:14 +0100

    fix

--- function.js
+++ function.js
@@ -1,4 +1,4 @@
-const allProduct = document.getElementById('all-product')
+// const allProduct = document.getElementById('all-product')
 const dailyProduct = document.getElementById('daily-product');
 const antiAging = document.getElementById('anti-aging');
 const cleansersProduct = document.getElementById('cleansers-product');
@@ -9,19 +9,26 @@ const antiAgingContent = document.getElementById('anti-aging-content');
 const cleansersProductContent = document.getElementById('cleansers-product-content');
 const allProductContent = document.getElementById('all-product-content');
 
+// Project Section start
+const mainProductImage = document.getElementById('main-product-image');
+const imageOne = document.getElementById('image-one');
+const imagetwo = document.getElementById('image-two');
+const imageThree = document.getElementById('image-three');
+const imageFour = document.getElementById('image-four');
+// 
 
-allProduct.addEventListener('click', () => {
-    allProduct.style.display = 'block';
-    dailyProductContent.style.display = 'none';
-    antiAgingContent.style.display = 'none';
-    cleansersProductContent.style.display = 'none';
-});
+// allProduct.addEventListener('click', () => {
+//     allProduct.style.display = 'block';
+//     dailyProductContent.style.display = 'none';
+//     antiAgingContent.style.display = 'none';
+//     cleansersProductContent.style.display = 'none';
+// });
 
 dailyProduct.addEventListener('click', () => {
     dailyProductContent.style.display = 'block';
     antiAgingContent.style.display = 'none';
     cleansersProductContent.style.display = 'none';
-    allProduct.style.display = 'none';
+    // allProduct.style.display = 'none';
 
 });
 
@@ -29,13 +36,13 @@ antiAging.addEventListener('click', () => {
     dailyProductContent.style.display = 'none';
     antiAgingContent.style.display = 'block';
     cleansersProductContent.style.display = 'none';
-    allProduct.style.display = 'none';
+    // allProduct.style.display = 'none';
 
 });
 
 cleansersProduct.addEventListener('click', () => {
     dailyProductContent.style.display = 'none';
     antiAgingContent.style.display = 'none';
-    allProduct.style.display = 'none';
+    // allProduct.style.display = 'none';
     cleansersProductContent.style.display = 'block';
 });
