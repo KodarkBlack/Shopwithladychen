28ab997bd4d1e6b55a820bb0601ec0454030d001
Olufisayo Israel Tomide <115098511+KodarkBlack@users.noreply.github.com>
2023-08-01 20:54:56 +0100

    fix

--- function.js
+++ function.js
@@ -3,9 +3,7 @@ const dailyProduct = document.getElementById('daily-product');
 const antiAging = document.getElementById('anti-aging');
 const cleansersProduct = document.getElementById('cleansers-product');
 
-const addToCartBtn = document.getElementById('add-to-cart-btn');
-const alertMessage = document.getElementById('alert-message');
-const addedCart = document.getElementById('added-cart');
+
 
 const dailyProductContent = document.getElementById('daily-product-content');
 const antiAgingContent = document.getElementById('anti-aging-content');
@@ -29,6 +27,7 @@ const imageFour = document.getElementById('image-four');
 
 dailyProduct.addEventListener('click', () => {
     dailyProductContent.style.display = 'block';
+    // dailyProductContent.style.color = '#BFBBAF'
     antiAgingContent.style.display = 'none';
     cleansersProductContent.style.display = 'none';
     // allProduct.style.display = 'none';
@@ -49,3 +48,48 @@ cleansersProduct.addEventListener('click', () => {
     // allProduct.style.display = 'none';
     cleansersProductContent.style.display = 'block';
 });
+
+
+// Initialize an empty cart
+let cartItems = [];
+
+// Function to add an item to the cart
+function addToCart(productName, price, quantity) {
+    // Check if the product already exists in the cart
+    const existingItem = cartItems.find(item => item.name === productName);
+    if (existingItem) {
+        existingItem.quantity += quantity;
+    } else {
+        cartItems.push({ name: productName, price: price, quantity: quantity });
+    }
+    updateCart();
+}
+
+// Function to update the cart UI
+function updateCart() {
+    const cartList = document.getElementById("added-cart");
+    const alertMessage = document.getElementById("alert-message");
+
+    // Clear the previous cart content
+    cartList.innerHTML = "";
+
+    // Rebuild the cart with the updated items
+    cartItems.forEach(item => {
+        const li = document.createElement("li");
+        li.innerText = `${item.name} - ${item.quantity} x ${item.price} NGN`;
+        cartList.appendChild(li);
+    });
+
+    // Display a message when the cart is empty
+    alertMessage.style.display = cartItems.length === 0 ? "block" : "none";
+}
+
+// Function to handle the form submission
+document.getElementById("add-to-cart-btn").addEventListener("click", function (event) {
+    event.preventDefault();
+    const productName = "Skin Care"; // Change this to the actual product name
+    const price = 59.00; // Change this to the actual product price
+    const quantity = parseInt(document.getElementById("input").value) || 1;
+    addToCart(productName, price, quantity);
+});
+
